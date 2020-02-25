a38bc8cc81b50ea6145c4cf018037e2279f31085
stephanie sassier <stephanie.sassier@gmail.com>
2020-02-25 18:54:15 +0100

    create en construction

--- app/controllers/child/tasks_controller.rb
+++ app/controllers/child/tasks_controller.rb
@@ -1,14 +1,23 @@
 class Child::TasksController < Child::ApplicationController
   def new
-
+    @category = Category.find(params[:category_id])
+    @task = Task.new
   end
 
   def create
     @task = Task.new(task_params)
+    @category = Category.find(params[:category_id])
+    @task.category = @task
       if @task.save
         redirect_to child_user_path(current_user)
       else
         render :new
     end
+  end
+
+  private
+
+  def review_params
+    params.require(:task).permit(:name, :description, :category_id)
   end
 end
