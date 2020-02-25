6bcae2d3fb6cfe8b92959195a1d9736d1019ca74
Lucile Lestrade <lucilecharly.lestrade@gmail.com>
2020-02-25 22:00:03 +0100

    seed achievements + display achievements done

--- app/controllers/parent/achievements_controller.rb
+++ app/controllers/parent/achievements_controller.rb
@@ -1,3 +1,30 @@
 class Parent::AchievementsController < Parent::ApplicationController
+  def new
+
+  end
+
+  def create
+
+  end
+
+  def edit
+    @achievement = Achievement.find(params[:id])
+  end
+
+  def update
+    @achievement = Achievement.find(params[:id])
+    @achievement.update(achievement_params)
+    redirect_to parent_user_path(current_user)
+  end
+
+  def destroy
+
+  end
+
+  private
+
+  def achievement_params
+    params.require(:achievement).permit(:task_id, :user_id, :points, :achieve)
+  end
 
 end
