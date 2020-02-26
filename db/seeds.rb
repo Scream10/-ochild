d4b599bc8a5f97e8e9ef761955c62b242568fff4
Lucile Lestrade <lucilecharly.lestrade@gmail.com>
2020-02-26 20:32:13 +0100

    seed en cours

--- db/seeds.rb
+++ db/seeds.rb
@@ -21,130 +21,106 @@ family = Family.new(name: "dupont")
 
 puts "Creating chores..."
 
-file = URI.open('https://res.cloudinary.com/dz8itoyac/image/upload/v1582304003/G9kW3HTMFcsxdvG3bVHV1oXN.png')
-task_one = Task.new(name: "Dishes", description: "wash the dishes", category: chores)
-puts "test 1"
-task_one.photo.attach(io: file, filename: 'dishes_zrctlq.png', content_type: 'image/png')
-task_one.save!
-puts "test 2"
 
-file = URI.open('https://res.cloudinary.com/dz8itoyac/image/upload/v1582727925/OCHILD/car_wash_ekyaej.svg')
+task_one = Task.new(name: "Dishes", description: "wash the dishes", category: chores)
+task_one.photo.attach(io: File.open('./app/assets/images/dishes.svg'), filename: 'dishes.svg')
+task_one.save!
+
 task_two = Task.new(name: "Car Wash", description: "clean the car", category: chores)
-task_two.photo.attach(io: file, filename: 'car_wash_ekyaej.svg', content_type: 'image/svg')
+task_one.photo.attach(io: File.open('./app/assets/images/car_wash.svg'), filename: 'car_wash.svg')
 task_two.save!
 
-file = URI.open('https://res.cloudinary.com/dz8itoyac/image/upload/v1582727949/OCHILD/washing_basket_ueacsb.svg')
 task = Task.new(name: "Laundry", description: "fold the laundry", category: chores)
-task.photo.attach(io: file, filename: 'washing_basket_ueacsb.svg', content_type: 'image/svg')
+task_one.photo.attach(io: File.open('./app/assets/images/washing_basket.svg'), filename: 'washing_basket.svg')
 task.save!
 
-file = URI.open('https://res.cloudinary.com/dz8itoyac/image/upload/v1582727951/OCHILD/window_cleaner_awr7ls.svg')
 task = Task.new(name: "Cleaning", description: "clean the windows", category: chores)
-task.photo.attach(io: file, filename: 'window_cleaner_awr7ls.svg', content_type: 'image/svg')
+task_one.photo.attach(io: File.open('./app/assets/images/window_cleaner.svg'), filename: 'window_cleaner.svg')
 task.save!
 
-file = URI.open('https://res.cloudinary.com/dz8itoyac/image/upload/v1582727949/OCHILD/toilet_rj9cra.svg')
 task = Task.new(name: "Toilet", description: "clean the toilets", category: chores)
-task.photo.attach(io: file, filename: 'toilet_rj9cra.svg', content_type: 'image/svg')
+task_one.photo.attach(io: File.open('./app/assets/images/toilet.svg'), filename: 'toilet.svg')
 task.save!
 
-file = URI.open('https://res.cloudinary.com/dz8itoyac/image/upload/v1582727949/OCHILD/washing_machine_nzg7tm.svg')
 task = Task.new(name: "Washing Machine", description: "wash your clothes", category: chores)
-task.photo.attach(io: file, filename: 'washing_machine_nzg7tm.svg', content_type: 'image/svg')
+task_one.photo.attach(io: File.open('./app/assets/images/washing_machine.svg'), filename: 'washing_machine.svg')
 task.save!
 
-file = URI.open('https://res.cloudinary.com/dz8itoyac/image/upload/v1582727949/OCHILD/vacuum_cleaner_do35l4.svg')
 task = Task.new(name: "Vacuum", description: "vacuum your room", category: chores)
-task.photo.attach(io: file, filename: 'vacuum_cleaner_do35l4.svg', content_type: 'image/svg')
+task_one.photo.attach(io: File.open('./app/assets/images/vacuum_cleaner.svg'), filename: 'vacuum_cleaner.svg')
 task.save!
 
-file = URI.open('https://res.cloudinary.com/dz8itoyac/image/upload/v1582727949/OCHILD/vacuum_cleaner_do35l4.svg')
 task = Task.new(name: "Vacuum", description: "vacuum your house", category: chores)
-task.photo.attach(io: file, filename: 'vacuum_cleaner_do35l4.svg', content_type: 'image/svg')
+task_one.photo.attach(io: File.open('./app/assets/images/vacuum_cleaner.svg'), filename: 'vacuum_cleaner.svg')
 task.save!
 
-file = URI.open('https://res.cloudinary.com/dz8itoyac/image/upload/v1582727949/OCHILD/trash_crv5mu.svg')
-task = Task.new(name: "Bin", description: "take the bins out", category: chores)
-task.photo.attach(io: file, filename: 'trash_crv5mu.svg', content_type: 'image/svg')
+task = Task.new(name: "Trash", description: "take the bins out", category: chores)
+task_one.photo.attach(io: File.open('./app/assets/images/trash.svg'), filename: 'trash.svg')
 task.save!
 
-file = URI.open('https://res.cloudinary.com/dz8itoyac/image/upload/v1582727928/OCHILD/setting_laundry_cgfsdo.svg')
 task = Task.new(name: "Laundry", description: "hangs the washing up", category: chores)
-task.photo.attach(io: file, filename: 'setting_laundry_cgfsdo.svg', content_type: 'image/svg')
+task_one.photo.attach(io: File.open('./app/assets/images/setting_laundry.svg'), filename: 'setting_laundry.svg')
 task.save!
 
-file = URI.open('https://res.cloudinary.com/dz8itoyac/image/upload/v1582727949/OCHILD/water_can_jougsi.svg')
 task = Task.new(name: "Gardening", description: "water the plants", category: chores)
-task.photo.attach(io: file, filename: 'water_can_jougsi.svg', content_type: 'image/svg')
+task_one.photo.attach(io: File.open('./app/assets/images/water_can.svg'), filename: 'water_can.svg')
 task.save!
 
-file = URI.open('https://res.cloudinary.com/dz8itoyac/image/upload/v1582727928/OCHILD/sink_dqtkjd.svg')
 task = Task.new(name: "Cleaning", description: "clean the bathroom", category: chores)
-task.photo.attach(io: file, filename: 'sink_dqtkjd.svg', content_type: 'image/svg')
+task_one.photo.attach(io: File.open('./app/assets/images/sink.svg'), filename: 'sink.svg')
 task.save!
 
-file = URI.open('https://res.cloudinary.com/dz8itoyac/image/upload/v1582727928/OCHILD/mop_lco7jh.svg')
 task = Task.new(name: "Cleaning", description: "mop the floor", category: chores)
-task.photo.attach(io: file, filename: 'mop_lco7jh.svg', content_type: 'image/svg')
+task_one.photo.attach(io: File.open('./app/assets/images/mop.svg'), filename: 'mop.svg')
 task.save!
 
-file = URI.open('https://res.cloudinary.com/dz8itoyac/image/upload/v1582727927/OCHILD/housekeeping_ltefib.svg')
 task = Task.new(name: "Cleaning", description: "tidy your house", category: chores)
-task.photo.attach(io: file, filename: 'housekeeping_ltefib.svg', content_type: 'image/svg')
+task_one.photo.attach(io: File.open('./app/assets/images/housekeeping.svg'), filename: 'housekeeping.svg')
 task.save!
 
-file = URI.open('https://res.cloudinary.com/dz8itoyac/image/upload/v1582727927/OCHILD/iron_lrxiie.svg')
 task = Task.new(name: "Ironning", description: "iron your clothes", category: chores)
-task.photo.attach(io: file, filename: 'iron_lrxiie.svg', content_type: 'image/svg')
+task_one.photo.attach(io: File.open('./app/assets/images/iron.svg'), filename: 'iron.svg')
 task.save!
 
-file = URI.open('https://res.cloudinary.com/dz8itoyac/image/upload/v1582727926/OCHILD/clean_shoes_slje4v.svg')
 task = Task.new(name: "Cleaning", description: "clean your shoes", category: chores)
-task.photo.attach(io: file, filename: 'clean_shoes_slje4v.svg', content_type: 'image/svg')
+task_one.photo.attach(io: File.open('./app/assets/images/clean_shoes.svg'), filename: 'clean_shoes.svg')
 task.save!
 
-file = URI.open('https://res.cloudinary.com/dz8itoyac/image/upload/v1582727926/OCHILD/cleaning_set_rjmjvp.svg')
 task = Task.new(name: "Cleaning", description: "clean the kitchen", category: chores)
-task.photo.attach(io: file, filename: 'cleaning_set_rjmjvp.svg', content_type: 'image/svg')
+task_one.photo.attach(io: File.open('./app/assets/images/cleaning_set.svg'), filename: 'cleaning_set.svg')
 task.save!
 
-file = URI.open('https://res.cloudinary.com/dz8itoyac/image/upload/v1582727926/OCHILD/cleaning_desk_h6r6gs.svg')
 task = Task.new(name: "Cleaning", description: "tidy your room", category: chores)
-task.photo.attach(io: file, filename: 'cleaning_desk_h6r6gs.svg', content_type: 'image/svg')
+task_one.photo.attach(io: File.open('./app/assets/images/cleaning_desk.svg'), filename: 'cleaning_desk.svg')
 task.save!
 
-file = URI.open('https://res.cloudinary.com/dz8itoyac/image/upload/v1582727913/OCHILD/broom_lhlhrn.svg')
 task = Task.new(name: "Cleaning", description: "clean the house", category: chores)
-task.photo.attach(io: file, filename: 'broom_lhlhrn.svg', content_type: 'image/svg')
+task_one.photo.attach(io: File.open('./app/assets/images/broom.svg'), filename: 'broom.svg')
 task.save!
 
 puts "Creating grades..."
 
-file = URI.open('https://res.cloudinary.com/dz8itoyac/image/upload/v1582729922/OCHILD/grade_h4llnv.svg')
 task = Task.new(name: "History", description: "new grade in history", category: grades)
-task.photo.attach(io: file, filename: 'grade_h4llnv.svg', content_type: 'image/svg')
+task_one.photo.attach(io: File.open('./app/assets/images/grade.svg'), filename: 'grade.svg')
 task.save!
 
-file = URI.open('https://res.cloudinary.com/dz8itoyac/image/upload/v1582729922/OCHILD/grade_h4llnv.svg')
 task = Task.new(name: "Litterature", description: "new grade in litterature", category: grades)
-task.photo.attach(io: file, filename: 'grade_h4llnv.svg', content_type: 'image/svg')
+task_one.photo.attach(io: File.open('./app/assets/images/grade.svg'), filename: 'grade.svg')
 task.save!
 
-file = URI.open('https://res.cloudinary.com/dz8itoyac/image/upload/v1582729922/OCHILD/grade_h4llnv.svg')
 task = Task.new(name: "Math", description: "new grade in math", category: grades)
-task.photo.attach(io: file, filename: 'grade_h4llnv.svg', content_type: 'image/svg')
+task_one.photo.attach(io: File.open('./app/assets/images/grade.svg'), filename: 'grade.svg')
 task.save!
 
-file = URI.open('https://res.cloudinary.com/dz8itoyac/image/upload/v1582729922/OCHILD/grade_h4llnv.svg')
 task_three = Task.new(name: "Biology", description: "new grade in biology", category: grades)
-task_three.photo.attach(io: file, filename: 'grade_h4llnv.svg', content_type: 'image/svg')
+task_one.photo.attach(io: File.open('./app/assets/images/grade.svg'), filename: 'grade.svg')
 task_three.save!
 
 puts "Creating readings..."
 
-file = URI.open('https://res.cloudinary.com/dz8itoyac/image/upload/v1582730330/OCHILD/reading_tg0hty.svg')
 task = Task.new(name: "Reading", description: "reading a new book", category: readings)
-task.photo.attach(io: file, filename: 'reading_tg0hty.svg', content_type: 'image/svg')
+task_one.photo.attach(io: File.open('./app/assets/images/reading.svg'), filename: 'reading.svg')
+
 task.save!
 
 puts "Creating everything else..."
