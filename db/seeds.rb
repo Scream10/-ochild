e5fffacf7e14ab3d8a1b7149cb23a069bb6d1645
Lucile Lestrade <lucilecharly.lestrade@gmail.com>
2020-02-26 21:39:29 +0100

    seed png

--- db/seeds.rb
+++ db/seeds.rb
@@ -23,103 +23,103 @@ puts "Creating chores..."
 
 
 task_one = Task.new(name: "Dishes", description: "wash the dishes", category: chores)
-task_one.photo.attach(io: File.open('./app/assets/images/dishes.svg'), filename: 'dishes.svg')
+task_one.photo.attach(io: File.open('./app/assets/images/dishes.png'), filename: 'dishes.png')
 task_one.save!
 
 task_two = Task.new(name: "Car Wash", description: "clean the car", category: chores)
-task_one.photo.attach(io: File.open('./app/assets/images/car_wash.svg'), filename: 'car_wash.svg')
+task_one.photo.attach(io: File.open('./app/assets/images/car_wash.png'), filename: 'car_wash.png')
 task_two.save!
 
 task = Task.new(name: "Laundry", description: "fold the laundry", category: chores)
-task_one.photo.attach(io: File.open('./app/assets/images/washing_basket.svg'), filename: 'washing_basket.svg')
+task_one.photo.attach(io: File.open('./app/assets/images/washing_basket.png'), filename: 'washing_basket.png')
 task.save!
 
 task = Task.new(name: "Cleaning", description: "clean the windows", category: chores)
-task_one.photo.attach(io: File.open('./app/assets/images/window_cleaner.svg'), filename: 'window_cleaner.svg')
+task_one.photo.attach(io: File.open('./app/assets/images/window_cleaner.png'), filename: 'window_cleaner.png')
 task.save!
 
 task = Task.new(name: "Toilet", description: "clean the toilets", category: chores)
-task_one.photo.attach(io: File.open('./app/assets/images/toilet.svg'), filename: 'toilet.svg')
+task_one.photo.attach(io: File.open('./app/assets/images/toilet.png'), filename: 'toilet.png')
 task.save!
 
 task = Task.new(name: "Washing Machine", description: "wash your clothes", category: chores)
-task_one.photo.attach(io: File.open('./app/assets/images/washing_machine.svg'), filename: 'washing_machine.svg')
+task_one.photo.attach(io: File.open('./app/assets/images/washing_machine.png'), filename: 'washing_machine.png')
 task.save!
 
 task = Task.new(name: "Vacuum", description: "vacuum your room", category: chores)
-task_one.photo.attach(io: File.open('./app/assets/images/vacuum_cleaner.svg'), filename: 'vacuum_cleaner.svg')
+task_one.photo.attach(io: File.open('./app/assets/images/vacuum_cleaner.png'), filename: 'vacuum_cleaner.png')
 task.save!
 
 task = Task.new(name: "Vacuum", description: "vacuum your house", category: chores)
-task_one.photo.attach(io: File.open('./app/assets/images/vacuum_cleaner.svg'), filename: 'vacuum_cleaner.svg')
+task_one.photo.attach(io: File.open('./app/assets/images/vacuum_cleaner.png'), filename: 'vacuum_cleaner.png')
 task.save!
 
 task = Task.new(name: "Trash", description: "take the bins out", category: chores)
-task_one.photo.attach(io: File.open('./app/assets/images/trash.svg'), filename: 'trash.svg')
+task_one.photo.attach(io: File.open('./app/assets/images/trash.png'), filename: 'trash.png')
 task.save!
 
 task = Task.new(name: "Laundry", description: "hangs the washing up", category: chores)
-task_one.photo.attach(io: File.open('./app/assets/images/setting_laundry.svg'), filename: 'setting_laundry.svg')
+task_one.photo.attach(io: File.open('./app/assets/images/setting_laundry.png'), filename: 'setting_laundry.png')
 task.save!
 
 task = Task.new(name: "Gardening", description: "water the plants", category: chores)
-task_one.photo.attach(io: File.open('./app/assets/images/water_can.svg'), filename: 'water_can.svg')
+task_one.photo.attach(io: File.open('./app/assets/images/water_can.png'), filename: 'water_can.png')
 task.save!
 
 task = Task.new(name: "Cleaning", description: "clean the bathroom", category: chores)
-task_one.photo.attach(io: File.open('./app/assets/images/sink.svg'), filename: 'sink.svg')
+task_one.photo.attach(io: File.open('./app/assets/images/sink.png'), filename: 'sink.png')
 task.save!
 
 task = Task.new(name: "Cleaning", description: "mop the floor", category: chores)
-task_one.photo.attach(io: File.open('./app/assets/images/mop.svg'), filename: 'mop.svg')
+task_one.photo.attach(io: File.open('./app/assets/images/mop.png'), filename: 'mop.png')
 task.save!
 
 task = Task.new(name: "Cleaning", description: "tidy your house", category: chores)
-task_one.photo.attach(io: File.open('./app/assets/images/housekeeping.svg'), filename: 'housekeeping.svg')
+task_one.photo.attach(io: File.open('./app/assets/images/housekeeping.png'), filename: 'housekeeping.png')
 task.save!
 
 task = Task.new(name: "Ironning", description: "iron your clothes", category: chores)
-task_one.photo.attach(io: File.open('./app/assets/images/iron.svg'), filename: 'iron.svg')
+task_one.photo.attach(io: File.open('./app/assets/images/iron.png'), filename: 'iron.png')
 task.save!
 
 task = Task.new(name: "Cleaning", description: "clean your shoes", category: chores)
-task_one.photo.attach(io: File.open('./app/assets/images/clean_shoes.svg'), filename: 'clean_shoes.svg')
+task_one.photo.attach(io: File.open('./app/assets/images/clean_shoes.png'), filename: 'clean_shoes.png')
 task.save!
 
 task = Task.new(name: "Cleaning", description: "clean the kitchen", category: chores)
-task_one.photo.attach(io: File.open('./app/assets/images/cleaning_set.svg'), filename: 'cleaning_set.svg')
+task_one.photo.attach(io: File.open('./app/assets/images/cleaning_set.png'), filename: 'cleaning_set.png')
 task.save!
 
 task = Task.new(name: "Cleaning", description: "tidy your room", category: chores)
-task_one.photo.attach(io: File.open('./app/assets/images/cleaning_desk.svg'), filename: 'cleaning_desk.svg')
+task_one.photo.attach(io: File.open('./app/assets/images/cleaning_desk.png'), filename: 'cleaning_desk.png')
 task.save!
 
 task = Task.new(name: "Cleaning", description: "clean the house", category: chores)
-task_one.photo.attach(io: File.open('./app/assets/images/broom.svg'), filename: 'broom.svg')
+task_one.photo.attach(io: File.open('./app/assets/images/broom.png'), filename: 'broom.png')
 task.save!
 
 puts "Creating grades..."
 
 task = Task.new(name: "History", description: "new grade in history", category: grades)
-task_one.photo.attach(io: File.open('./app/assets/images/grade.svg'), filename: 'grade.svg')
+task_one.photo.attach(io: File.open('./app/assets/images/grade.png'), filename: 'grade.png')
 task.save!
 
 task = Task.new(name: "Litterature", description: "new grade in litterature", category: grades)
-task_one.photo.attach(io: File.open('./app/assets/images/grade.svg'), filename: 'grade.svg')
+task_one.photo.attach(io: File.open('./app/assets/images/grade.png'), filename: 'grade.png')
 task.save!
 
 task = Task.new(name: "Math", description: "new grade in math", category: grades)
-task_one.photo.attach(io: File.open('./app/assets/images/grade.svg'), filename: 'grade.svg')
+task_one.photo.attach(io: File.open('./app/assets/images/grade.png'), filename: 'grade.png')
 task.save!
 
 task_three = Task.new(name: "Biology", description: "new grade in biology", category: grades)
-task_one.photo.attach(io: File.open('./app/assets/images/grade.svg'), filename: 'grade.svg')
+task_one.photo.attach(io: File.open('./app/assets/images/grade.png'), filename: 'grade.png')
 task_three.save!
 
 puts "Creating readings..."
 
 task = Task.new(name: "Reading", description: "reading a new book", category: readings)
-task_one.photo.attach(io: File.open('./app/assets/images/reading.svg'), filename: 'reading.svg')
+task_one.photo.attach(io: File.open('./app/assets/images/reading.png'), filename: 'reading.png')
 
 task.save!
 
@@ -138,7 +138,6 @@ achievement_one = Achievement.new(due_at: "2020-02-28", achieve: false, done: fa
 achievement_two = Achievement.new(due_at: "2020-03-01", achieve: false, done: true, user: child, task: task_two, points: 35)
 achievement_three = Achievement.new(due_at: "2020-03-01", achieve: false, done: true, user: child, task: task_three, points: 18)
 
-# category.save
 family.save
 child.save
 parent.save
