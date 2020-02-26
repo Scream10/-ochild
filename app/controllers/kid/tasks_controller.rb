7fde607856015db2c04cf4b94ef215925fd46811
stephanie sassier <stephanie.sassier@gmail.com>
2020-02-26 12:34:27 +0100

    finsih

--- app/controllers/kid/tasks_controller.rb
+++ app/controllers/kid/tasks_controller.rb
@@ -1,12 +1,12 @@
-class Kid::TasksController < Kid::ApplicationController
+class Kid::TasksController < ApplicationController
   def new
-    @task = Task.new
+    @task = Task.new()
   end
 
   def create
     @task = Task.new(task_params)
       if @task.save
-        redirect_to child_user_path(current_user)
+        redirect_to kid_user_path(current_user)
       else
         render :new
     end
@@ -14,7 +14,7 @@ class Kid::TasksController < Kid::ApplicationController
 
   private
 
-  def review_params
+  def task_params
     params.require(:task).permit(:name, :description, :photo, :category_id)
   end
 end
