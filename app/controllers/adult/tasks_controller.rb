5c960a0d9f677029efc31795e2d0dc40c15a6e04
stephanie sassier <stephanie.sassier@gmail.com>
2020-02-26 12:56:44 +0100

    adult can create a task

--- app/controllers/adult/tasks_controller.rb
+++ app/controllers/adult/tasks_controller.rb
@@ -1,5 +1,16 @@
-class Adult::TasksController < Adult::ApplicationController
+class Adult::TasksController < ApplicationController
+def new
+    @task = Task.new()
+  end
 
+  def create
+    @task = Task.new(task_params)
+      if @task.save
+        redirect_to adult_user_path(current_user)
+      else
+        render :new
+    end
+  end
 
 private
 
