e7e30ab7a53f109553498511679125cd5df12c2a
Nicolas Diot <nicolasdiot10@gmail.com>
2020-02-28 12:28:41 +0100

    spinning update when child make a task

--- app/controllers/kid/users_controller.rb
+++ app/controllers/kid/users_controller.rb
@@ -9,5 +9,22 @@ class Kid::UsersController < ApplicationController
     @achievements = Achievement.all
     @count_achievement_done_true = current_user.achievements.where(done: true, achieve: false)
     @count_achievement_done_false = current_user.achievements.where(done: false, achieve: false)
+    @pourcent_goal_achieve = count_achievements
   end
+
+  private
+
+  def count_achievements
+    total_achievements = 0
+
+    @goal.each do |goal|
+      @achievements.each do |achievement|
+        total_achievements += achievement.points
+      end
+
+      result = total_achievements * 100 / goal.total_points
+      return result
+    end
+  end
+
 end
