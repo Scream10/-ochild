51aeedd6d6617d6d754c9a94be6179bba8d8b17c
Lucile Lestrade <lucilecharly.lestrade@gmail.com>
2020-02-29 20:48:38 +0100

    cleaning fichier js all good normalement ahah

--- app/controllers/adult/achievements_controller.rb
+++ app/controllers/adult/achievements_controller.rb
@@ -21,7 +21,6 @@ class Adult::AchievementsController < ApplicationController
   end
 
   def update
-    raise
     @achievement = Achievement.find(params[:id])
     @kid = @achievement.user
     @achievement.update(achievement_params)
--- app/javascript/components/plusMinus.js
+++ app/javascript/components/plusMinus.js
@@ -1,28 +1,6 @@
-
-    // const count = 1;
-    // const countEl = document.getElementById("count");
-    // const plus = () => {
-    //     count++;
-    //     countEl.value = count;
-    // };
-
-    // const minus = () => {
-    //   if (count > 1) {
-    //     count--;
-    //     countEl.value = count;
-    //   };
-    // };
-
-
-
-// let count = 1;
 const plus = document.querySelector('#plus');
 const raiseCount = (points) => {
   plus.addEventListener('click',(event) => {
-    // if( count > 1 ) {
-    //   count ++;
-      // points.value = count;
-    // };
     let total = points.value;
     let totalPoints = parseInt(total, 10);
     totalPoints += 1;
@@ -41,6 +19,3 @@ const downCount = (points) => {
 };
 
 export { raiseCount, downCount };
-
-// On veut une fonction qui lorsqu'on clique sur un bouton augmente la valeur
-// d'un élément ciblé
