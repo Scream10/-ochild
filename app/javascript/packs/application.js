2b2c1f8dbf0d8e53ab6161d500cf6e9da1f703c1
Lucile Lestrade <lucilecharly.lestrade@gmail.com>
2020-02-29 17:09:39 +0100

    modal fonctionne mais super moche

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,6 +1,7 @@
 import "bootstrap";
 // A BOUGER DANS LE BON FICHIER MAIS JE SAIS PAS LEQUEL...
 
+// CHECKBOX ACHIEVEMENTS
 import { validationAchievement } from '../components/validationAchievements.js';
 
 const achievementElements = document.querySelectorAll('.achievement-id');
@@ -10,3 +11,28 @@ if (achievementElements) {
     validationAchievement(achievementElement);
   })
 };
+
+// TEST MODAL NEW ACHIEVEMENT WITH TASK SUGGESTED
+
+const taskElements = document.querySelectorAll('.btn-modal');
+const overlay = document.getElementById('overlay');
+const modal = document.getElementById('modal-task');
+
+taskElements.forEach(taskElement => {
+  taskElement.addEventListener('click', (event) => {
+    overlay.classList.add('is-visible');
+    modal.classList.add('is-visible');
+  })
+});
+
+const closeBtn = document.getElementById('close-btn');
+
+closeBtn.addEventListener('click', (event) => {
+  overlay.classList.remove('is-visible');
+  modal.classList.remove('is-visible');
+});
+
+overlay.addEventListener('click', (event) => {
+  overlay.classList.remove('is-visible');
+  modal.classList.remove('is-visible');
+});
