04a984148cff8f5d18b97e298a260c23f3154e74
stephanie sassier <stephanie.sassier@gmail.com>
2020-03-04 09:58:39 +0100

    Changement de la date ok

--- db/seeds.rb
+++ db/seeds.rb
@@ -137,9 +137,9 @@ goal = Goal.new(title: "anniversaire Camille", end_date: "2020-03-03", total_poi
 proportion_one = Proportion.new(percent: 70, goal: goal, category: chores)
 proportion_two = Proportion.new(percent: 30, goal: goal, category: grades)
 
-achievement_one = Achievement.new(due_at: "2020-02-28", achieve: false, done: false, user: child, task: task_one, points: 25)
-achievement_two = Achievement.new(due_at: "2020-03-01", achieve: false, done: true, user: child, task: task_two, points: 35)
-achievement_three = Achievement.new(due_at: "2020-03-01", achieve: false, done: true, user: child, task: task_three, points: 18)
+achievement_one = Achievement.new(due_at: "Fri, 20 Mar 2020", achieve: false, done: false, user: child, task: task_one, points: 25)
+achievement_two = Achievement.new(due_at: "Fri, 10 Mar 2020", achieve: false, done: true, user: child, task: task_two, points: 35)
+achievement_three = Achievement.new(due_at: "Fri, 8 Mar 2020", achieve: false, done: true, user: child, task: task_three, points: 18)
 
 family.save
 child.save
