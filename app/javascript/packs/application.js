3bc876c915c07ae183f68269fbda0890a76fe7f7
Nicolas Diot <nicolasdiot10@gmail.com>
2020-03-05 19:58:57 +0100

    spin ok

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -14,10 +14,6 @@ modalNewAchievement();
 import { toggleDateInput } from '../components/toggleDateInput.js';
 toggleDateInput();
 
-// REGISTRATION LISTENER
-import { family_id } from '../components/registration_input.js';
-family_id();
-
 // Code très moche :
 
 
@@ -280,3 +276,10 @@ if (container) {
   // export { myCharts };
   myCharts()
 }
+
+
+// REGISTRATION LISTENER
+import { family_id } from '../components/registration_input.js';
+family_id();
+
+
